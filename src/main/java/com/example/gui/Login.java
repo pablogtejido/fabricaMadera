ae54d970a79444fb54ce7a3c2afef133b45e6b3a
aitorru <aitorruiz2m@gmail.com>
2021-04-19 18:38:59 +0200

    Borrar el main

--- src/main/java/com/example/gui/Login.java
+++ src/main/java/com/example/gui/Login.java
@@ -29,22 +29,6 @@ public class Login extends JFrame {
 	private final JLabel lblContrasena = new JLabel("Contraseña:");
 
 	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Login frame = new Login();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
 	 * Create the frame.
 	 */
 	public Login() {
