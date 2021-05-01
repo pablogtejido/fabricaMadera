c46a90308a97b97c610e5643b77298cff8ab40bd
pablogtejido <pablo.gonzaleztejido@opendeusto.es>
2021-05-01 22:15:22 +0200

    Performance test Online subir Clientes

---
+++ src/test/java/com/atenea/resource/ClienteResourcePerformanceTest.java
@@ -0,0 +1,14 @@
+package com.atenea.resource;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ClienteResourcePerformanceTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
